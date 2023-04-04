7e5215fe23bad464ece5f756f559770587191079
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-04-05 01:54:29 +0200

    task in progres...

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 import { PixabayAPI } from "./pixabay-api";
+import Notiflix from 'notiflix';
 
 const pixabayAPI = new PixabayAPI();
 
 const formEl = document.querySelector(`#search-form`);
-console.log(formEl)
 const searchBtnEl = document.querySelector(`button[type="submit"]`);
 const galleryEl = document.querySelector(`.gallery`);
+const loadMoreEl = document.querySelector(`.load-more`);
 
 function handleSearchPhotos(e) {
     e.preventDefault();
 
-    // const searchQuery = e.target.elements[`searchQuery`].value.trim();
-    // const searchQuery = formEl.elements.searchQuery.value.trim();
+    const searchQuery = e.target.elements.searchQuery.value.trim();
+    pixabayAPI.q = searchQuery;
 
-    console.log(searchQuery)
-    // pixabayAPI.q = searchQuery;
+    if (!pixabayAPI.q) {
+        return
+    }
 
-    pixabayAPI.fetchPhotos().then(data => console.log(data));
+    pixabayAPI.page += 1;
+    pixabayAPI.fetchPhotos()
+        .then(data => {
+            renderingGallery(data.hits);
+            loadMoreEl.classList.remove("is-hiden")
+        })
+        .catch(err => Notiflix.Notify.failure(err));
 }
 
-console.log(searchBtnEl)
-console.log(galleryEl)
+function renderingGallery(img) {
+    const markup = img.map(({ webformatURL, tags, likes, views, comments, downloads, previewWidth, previewHeight }) => `<div class="photo-card">
+<img width="200" height="200" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+        <p class="info-item">
+            <b>Likes:</b>${likes}
+        </p>
+        <p class="info-item">
+            <b>Views:</b>${views}
+        </p>
+        <p class="info-item">
+            <b>Comments:</b>${comments}
+        </p>
+        <p class="info-item">
+            <b>Downloads:</b>${downloads}
+        </p>
+    </div>
+</div>`).join('');
+    return galleryEl.insertAdjacentHTML(`beforeend`, markup)
+}
 
-searchBtnEl.addEventListener(`submit`, handleSearchPhotos);
+formEl.addEventListener(`submit`, handleSearchPhotos);
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,3 +1,5 @@
+import Notiflix from "notiflix";
+
 export class PixabayAPI {
     #BASE_URL = `https://pixabay.com/api/`;
     #API_KEY = `35043775-4ad51de805624d481f6825733`;
@@ -5,35 +7,28 @@ export class PixabayAPI {
     #BASE_SEARCH_PARAMS = {
         image_type: `photo`,
         orientation: `horizontal`,
-        safesearch: `true`
+        safesearch: `true`,
+        per_page: 40,
     }
 
-    // q = null;
-    q = `cat`;
-    page = 0;
-    imageType = `photo`;
-    orientation = `horizontal`;
-    safesearch = `true`;
-    perPage = 40;
+    q = null;
+    page = 1;
     
     fetchPhotos() {
-        // const searchParams = new URLSearchParams ({
-        //     q: this.q,            
-        //     ...this.#BASE_SEARCH_PARAMS,
-        //     page: this.page,
-        // })
+        const searchParams = new URLSearchParams ({
+            q: this.q,            
+            ...this.#BASE_SEARCH_PARAMS,
+            page: this.page,
+        })
 
-        return fetch(
-            // `${this.#BASE_URL}?key=${this.#API_KEY}&${searchParams}`
-            `${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.q}&image_type=${this.imageType}&orientation=${this.orientation}&safesearch=${this.safesearch}&per_page=${this.perPage}&page=${this.page}`
-            // `${this.#BASE_URL}?key=${this.#API_KEY}&${searchParams}`
-
-        ).then(data => {
-            if (!data.ok) {
-                throw new Error(`Oops... Something went wrong`);
-            }
-            console(data);
-            return data.json();
-        });
+        return fetch(`${this.#BASE_URL}?key=${this.#API_KEY}&${searchParams}`)
+            .then(data => {
+                if (!data.ok) {
+                    throw new Error(`Sorry, there are no images matching your search query. Please try again.`);
+                }
+                console.log(data.hits);
+                return data.json();
+            })
+            .catch(err => Notiflix.Notify.failure(err));
     }
 }
